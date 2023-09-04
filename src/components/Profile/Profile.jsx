ca9f4a8e7715f948b3decfaac5ee266c820c7456
mardhika wulansari <64689649+ichalatte@users.noreply.github.com>
2023-09-04 18:28:08 +0700

    responsive motto

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -228,18 +228,19 @@ const achievement = (position) => {
         </tbody>
       </table>
     </Card>
-    <Card style={{ width: '970px', height: '250px' }} className="mx-auto my-auto mt-5 relative bg-gray-200">
-    <Card style={{ width: '950px', height: '230px' }} className="mx-auto my-auto relative">
-    <CardBody>
-            <Typography variant="h4" color="blue-gray" className="mb-2 text-center">
-              Motto :
-            </Typography>
-            <Typography variant="h8" color="gray-200" className="mb-2 text-center">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </Typography>
-            </CardBody>
-    </Card>
-    </Card>
+    <div className="max-w-screen-xl mx-auto mt-5">
+  <div className="w-full md:w-10/11 lg:w-8/9 xl:w-7/9 mx-auto grid grid-cols-2 gap-4">
+    <div className="col-span-2 bg-gray-200 p-2 md:p-4 rounded-lg">
+      <div className="bg-white p-2 md:p-4 rounded-lg">
+        <h4 className="text-2xl font-semibold text-center mb-4">Motto :</h4>
+        <p className="text-sm md:text-base text-gray-600 text-center">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
       </div>
       </div>
        {/* Modal */}
@@ -269,7 +270,8 @@ const achievement = (position) => {
         >
           &times;
         </button>
-        <h2>Edit Profile</h2>
+        <h2 className="font-bold text-2xl">Edit Profile</h2>
+        <br />
         <div class="mb-4">
           <label for="email" class="block text-gray-500 font-bold mb-1">Email:</label>
           <input
