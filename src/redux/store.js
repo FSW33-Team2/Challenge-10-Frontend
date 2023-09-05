9da6762b47e6b84c9997ae78764a198179329e32
Shendy <Shendy1567@gmail.com>
2023-09-05 17:02:22 +0700

    adding dynamic button and dummy games

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
-import RPSGameSlice from "./features/RpsGameSlice";
-import LeaderboardListSlice from "./features/LeaderboardListSlice";
-import GamesByIdSlice from "./features/GamesByIdSlice";
+import { configureStore } from '@reduxjs/toolkit'
+import RPSGameSlice from './features/RpsGameSlice'
+import LeaderboardListSlice from './features/LeaderboardListSlice'
+import GamesByIdSlice from './features/GamesByIdSlice'
+import DynamicButton from './features/DynamicButton'
+import DummyGames from './features/DummyGames'
 
 export const store = configureStore({
   reducer: {
     rpsgame: RPSGameSlice,
     leaderboard: LeaderboardListSlice,
     allgames: GamesByIdSlice,
+    button: DynamicButton,
+    DummyGames,
   },
-});
+})
